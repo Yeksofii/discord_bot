cbaac13b3b99c319e0c2eb77928f4c1bdde8d7ee
Yeksofii <yeksofii@gmail.com>
2025-11-16 22:01:35 +0500

    Update config.py

--- config.py
+++ config.py
@@ -7,6 +7,7 @@ AUTO_ROLE_ID = 1438950808019271690
 
 # ---------- Ticket System Config ----------
 TICKET_CATEGORY_ID = 1439589764222423160    # Category where tickets are created
-STAFF_ROLE_ID = 987654321098765432          # Role ID for staff/support
+STAFF_ROLE_ID = 1438950739664830534          # Role ID for staff/support
 TRANSCRIPT_LOG_CHANNEL_ID = 1439656270000033944  # Channel for ticket transcripts
 TICKET_PANEL_CHANNEL_ID = 1439589764222423160   # Channel where the Open Ticket panel button appears
+
